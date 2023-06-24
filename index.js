7716fd18576d877e1c07af239404a2d309fd4d0d
Sunil Joshi <joshisunil2601@gmail.com>
2023-06-24 20:55:39 +0530

    server done

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import express from "express";
 import postRoutes from "./routes/posts.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/user.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api/post", postRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+
 app.listen(8000, () => {
   console.log("connected");
 });
