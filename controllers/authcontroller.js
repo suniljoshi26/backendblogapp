2d55e796240821affe9bb48e8898c45211827256
Sunil Joshi <joshisunil2601@gmail.com>
2023-06-25 14:26:06 +0530

    login api

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,5 +1,6 @@
 import { db } from "../db.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 export const register = (req, res) => {
   // console.log(req.body);
   //CHECK EXISTING USER
@@ -23,5 +24,28 @@ export const register = (req, res) => {
     });
   });
 };
-export const login = (req, res) => {};
+export const login = (req, res) => {
+  const q = "SELECT * FROM user WHERE username = ?";
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.json(err);
+    if (data.length === 0) return res.status(404).json("User not found!");
+    //check password
+
+    const isPasswordCorrect = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+    if (!isPasswordCorrect) {
+      return res.status(400).json("Wrong username or password");
+    }
+    const token = jwt.sign({ id: data[0].id }, "jwtkey");
+    const { password, ...other } = data[0];
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(other);
+  });
+};
 export const logout = (req, res) => {};
--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import express from "express";
 import postRoutes from "./routes/posts.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/user.js";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/post", postRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
