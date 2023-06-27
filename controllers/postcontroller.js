dc8b56a75b015197435198e120d7d62f21b7ece8
Sunil Joshi <joshisunil2601@gmail.com>
2023-06-27 20:49:04 +0530

    get post with id

--- controllers/postcontroller.js
+++ controllers/postcontroller.js
@@ -3,7 +3,7 @@ import { db } from "../db.js";
 export const getPosts = (req, res) => {
   const q = req.query.cat
     ? "SELECT * FROM posts WHERE cat=?"
-    : "SELECT * FROM posts";
+    : "SELECT  * FROM posts";
   db.query(q, [req.query.cat], (err, data) => {
     if (err) return res.send(err);
     return res.status(200).json(data);
@@ -11,7 +11,14 @@ export const getPosts = (req, res) => {
 };
 
 export const getPost = (req, res) => {
-  res.json("from controller");
+  console.log(req.params);
+  const q =
+    "SELECT  `username`,`title` ,`des`,p.img,`cat`,`date` FROM user u JOIN posts p ON u.id=p.userId WHERE  p.id = ?";
+  console.log(q);
+  db.query(q, [req.params.id], (err, data) => {
+    if (err) return res.json(err);
+    return res.status(200).json(data[0]);
+  });
 };
 export const addPost = (req, res) => {
   res.json("from controller");
