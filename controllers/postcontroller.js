1817eac20850720ebb6dd297b25b5a428dae2902
Sunil Joshi <joshisunil2601@gmail.com>
2023-06-28 20:20:16 +0530

    res

--- controllers/postcontroller.js
+++ controllers/postcontroller.js
@@ -1,4 +1,5 @@
 import { db } from "../db.js";
+import jwt from "jsonwebtoken";
 
 export const getPosts = (req, res) => {
   const q = req.query.cat
@@ -24,7 +25,19 @@ export const addPost = (req, res) => {
   res.json("from controller");
 };
 export const deletePost = (req, res) => {
-  res.json("from controller");
+  const token = req.cookies.access_token;
+  if (!token) return res.status(401).json("Not Authenticated");
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+    if (err) return rex.status(403).json("Token is not Valid!");
+
+    const postId = res.params.id;
+    const q = "DELETE FORM posts WHERE `id` =? and `userId`=?";
+    db.query(q, [postId, userInfo.id], (err, data) => {
+      if (err) return res.status(403).json("You can delete only your post");
+
+      return res.json("Post has been deleted! ");
+    });
+  });
 };
 export const updatePost = (req, res) => {
   res.json("from controller");
